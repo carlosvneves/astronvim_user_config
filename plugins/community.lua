6ee928dba50a4c06523b0e72c238d0f8bf2ce0dc
carlos@juno <carlos.vneves@gmail.com>
2023-11-11 15:37:16 -0300

    first config

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,29 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- Language packs
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.quarto" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.json" },
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- utilities
+  { import = "astrocommunity.motion.harpoon" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.motion.nvim-surround" },
+
+  -- color
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.color.transparent-nvim" },
+  "nyoom-engineering/oxocarbon.nvim",
+  "oneslash/helix-nvim",
 }
