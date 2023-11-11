fd8cd8840a7e81f6c03c1f7ba6613bf15bc4a73c
carlos@juno <carlos.vneves@gmail.com>
2023-11-11 16:20:20 -0300

    quarto config

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local utils = require "astronvim.utils"
 return {
   -- Configure AstroNvim updates
   updater = {
@@ -82,4 +83,38 @@ return {
     --   },
     -- }
   end,
+  -- Quarto plugin config
+  {
+    "quarto-dev/quarto-nvim",
+    ft = { "quarto", "qmd" },
+    opts = {},
+    dependencies = {
+      { "hrsh7th/nvim-cmp" },
+      { "jmbuhr/otter.nvim" },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      local ensure_installed = {
+        "r",
+        "python",
+        "markdown",
+        "markdown_inline",
+        "julia",
+        "bash",
+        "yaml",
+        "lua",
+        "vim",
+        "query",
+        "vimdoc",
+        "latex",
+        "html",
+        "css",
+      }
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = utils.list_insert_unique(opts.ensure_installed, ensure_installed)
+      end
+    end,
+  },
 }
